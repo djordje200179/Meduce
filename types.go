95dad7187a5079a3b8f2da349cc40bea91d4c394
Đorđe Milanović <djordje200179@gmail.com>
2023-04-25 16:09:19 +0200

    Minor fix of finalizer

--- types.go
+++ types.go
@@ -6,7 +6,7 @@ type Source[KeyIn, ValueIn any] <-chan misc.Pair[KeyIn, ValueIn]
 type Emitter[KeyOut, ValueOut any] func(key KeyOut, value ValueOut)
 type Mapper[KeyIn, ValueIn, KeyOut, ValueOut any] func(key KeyIn, value ValueIn, emit Emitter[KeyOut, ValueOut])
 type Reducer[KeyOut, ValueOut any] func(key KeyOut, values []ValueOut) ValueOut
-type Finalizer[KeyOut, ValueOut any] func(key KeyOut, valueRef *ValueOut) ValueOut
+type Finalizer[KeyOut, ValueOut any] func(key KeyOut, valueRef *ValueOut)
 
 type Collector[KeyOut, ValueOut any] interface {
 	Collect(key KeyOut, value ValueOut)
