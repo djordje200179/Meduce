7feb8e837fa3ad0bdaaf1dc4cc51f238b8040fb3
Đorđe Milanović <djordje200179@gmail.com>
2023-04-25 23:27:12 +0200

    Collector simplified

--- collectors/to_map.go
+++ collectors/to_map.go
@@ -2,20 +2,14 @@ package collectors
 
 import "github.com/djordje200179/meduce"
 
-type MapCollector[KeyOut comparable, ValueOut any] struct {
-	m map[KeyOut]ValueOut
-}
+type MapCollector[KeyOut comparable, ValueOut any] map[KeyOut]ValueOut
 
 func NewMapCollector[KeyOut comparable, ValueOut any]() meduce.Collector[KeyOut, ValueOut] {
-	collector := MapCollector[KeyOut, ValueOut]{
-		m: make(map[KeyOut]ValueOut),
-	}
-
-	return collector
+	return make(MapCollector[KeyOut, ValueOut])
 }
 
 func (collector MapCollector[KeyOut, ValueOut]) Collect(key KeyOut, value ValueOut) {
-	collector.m[key] = value
+	collector[key] = value
 }
 
 func (collector MapCollector[KeyOut, ValueOut]) Finalize() {
