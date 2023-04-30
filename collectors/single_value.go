a53d44949291c012de739855c6048047b7374068
Đorđe Milanović <djordje200179@gmail.com>
2023-04-30 23:44:04 +0200

    Single value collector reorganized

--- collectors/single_value.go
+++ collectors/single_value.go
@@ -2,23 +2,18 @@ package collectors
 
 // SingleValueCollector is a collector that collects a single value.
 //
-// Zero value of SingleValueCollector is a valid collector
-// that collects a value for the zero value of the key type.
+// Zero value of SingleValueCollector is a valid collector.
 type SingleValueCollector[KeyOut comparable, ValueOut any] struct {
 	set bool
 
-	wantedKey KeyOut
-	value     ValueOut
+	key   KeyOut
+	value ValueOut
 }
 
 // NewSingleValueCollector creates a new SingleValueCollector
 // that collects a value for the given key.
-func NewSingleValueCollector[KeyOut comparable, ValueOut any](wantedKey KeyOut) *SingleValueCollector[KeyOut, ValueOut] {
-	collector := &SingleValueCollector[KeyOut, ValueOut]{
-		wantedKey: wantedKey,
-	}
-
-	return collector
+func NewSingleValueCollector[KeyOut comparable, ValueOut any]() *SingleValueCollector[KeyOut, ValueOut] {
+	return &SingleValueCollector[KeyOut, ValueOut]{}
 }
 
 func (collector *SingleValueCollector[KeyOut, ValueOut]) Init() {
@@ -26,32 +21,45 @@ func (collector *SingleValueCollector[KeyOut, ValueOut]) Init() {
 }
 
 func (collector *SingleValueCollector[KeyOut, ValueOut]) Collect(key KeyOut, value ValueOut) {
-	if key == collector.wantedKey {
-		collector.value = value
-		collector.set = true
+	if collector.set {
+		panic("Already collected a value")
 	}
+
+	collector.set = true
+	collector.key = key
+	collector.value = value
 }
 
 func (collector *SingleValueCollector[KeyOut, ValueOut]) Finalize() {
 
 }
 
-// TryGet returns the collected value and true if the value was collected.
-// If the value was not collected, it returns the default value and false.
-func (collector *SingleValueCollector[KeyOut, ValueOut]) TryGet() (ValueOut, bool) {
-	if collector.set {
-		return collector.value, true
-	} else {
-		return collector.value, false
+// Get returns the collected key-value pair.
+func (collector *SingleValueCollector[KeyOut, ValueOut]) Get() (KeyOut, ValueOut) {
+	if !collector.set {
+		panic("No value collected")
 	}
+
+	return collector.key, collector.value
 }
 
-// Get returns the collected value.
-// If the value was not collected, it panics.
-func (collector *SingleValueCollector[KeyOut, ValueOut]) Get() ValueOut {
-	if collector.set {
-		return collector.value
-	} else {
-		panic("Value was not set")
+func (collector *SingleValueCollector[KeyOut, ValueOut]) Key() KeyOut {
+	if !collector.set {
+		panic("No value collected")
 	}
+
+	return collector.key
+}
+
+func (collector *SingleValueCollector[KeyOut, ValueOut]) Value() ValueOut {
+	if !collector.set {
+		panic("No value collected")
+	}
+
+	return collector.value
+}
+
+// IsSet returns true if a key-value pair was collected.
+func (collector *SingleValueCollector[KeyOut, ValueOut]) IsSet() bool {
+	return collector.set
 }
